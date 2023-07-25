ea7459ee3a8d41485d9aa113c46f69d1a63faf3a
ishiekh-dev <ishiekh.dev.v1@gmail.com>
2023-07-25 03:16:57 +0300

    setup script types

---
+++ HimaMage/SetupExample/Setup/UpgradeData.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HimaMage\SetupExample\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements  UpgradeDataInterface
+{
+
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        echo  __METHOD__.PHP_EOL;
+    }
+}
---
+++ HimaMage/SetupExample/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'HimaMage_SetupExample', __DIR__);
